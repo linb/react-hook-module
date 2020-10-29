19900bff5aec72a6edbfbf3feca649e57fced9e8
Jack Li <linb.net@gmail.com>
2020-10-29 02:16:20 -0400

    Add files via upload

--- plugin_router.js
+++ plugin_router.js
@@ -4,33 +4,31 @@ import { useModule } from "./";
 export * from "react-router-dom";
 
 const RelativeRouter = ({ children, _usemodule_in_design }) =>{
-  const routes = [];
   if(!Array.isArray(children))children=[children];
   if(!_usemodule_in_design){
+    const routes = [];
     children.forEach(child => {
       if(child.props && child.props.path && typeof(child.props.path)==="string"){
         routes.push({ path: child.props.path, element : child });
       }
     });
-  }
-  let result = useRoutes(routes);
-  if(_usemodule_in_design){
+    return useRoutes(routes);
+  }else{
     children.forEach(child => {
       if(child.props && Array.isArray(child.props.children)){
-        child.props.children = child.props.children.map( c =>
-          (
-            c.props && c.props.path && !c.props._usemodule_lastsel
-            ? React.createElement("div",{style:{border:'dotted #444 1px',padding:"4px",margin:"4px"}},"{ path : '" + c.props.path + "', component: <"+c.props._usemodule_vartag+" ... >...</"+c.props._usemodule_vartag+"> }")
-            : /^[AB]_/.test(c.props.id)
-              ? c
-              : React.createElement("div",{style:{border:'dotted #444 1px',padding:"4px",margin:"4px"}}, c)
-          )
-        )
+        child.props.children = child.props.children.map( c => {
+            const { children, ...others } = c.props; 
+            return (c.props && !/^[AB]_/.test(c.props.id) && c.props.path) ? React.createElement( "div",
+              { ...others, style:{border:'dotted #444 1px',padding:"4px",margin:"4px",background:(c.props._usemodule_lastsel?"#fffbd4":"")}},
+              !c.props._usemodule_lastsel
+                ? "{ path : '" + (c.props.path||"") + "', title : '" + (c.props.title||"") + "', component: <"+c.props._usemodule_vartag+">" + (children&&children.length===1&&children[0]&&children[0].type==='usemoduletext'?children[0].props.children[0]:"...") + "</"+c.props._usemodule_vartag+"> }" 
+                : c
+            ) : c;
+          })
       }
     });
-    result = React.createElement("div", {style:{"font-size":"75%",border:'dashed #444 1px',padding:"4px",margin:"4px"}}, children);
+    return React.createElement("div", {style:{"font-size":"75%",border:'dashed #444 1px',padding:"4px",margin:"4px"}}, children);
   }
-  return result;
 };
 
 const useRouter = () => {
