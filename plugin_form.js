ec406c8242396f24336d73d121f7b84b61a5250d
Jack Li <linb.net@gmail.com>
2020-11-15 09:53:53 -0500

    Add files via upload

--- plugin_form.js
+++ plugin_form.js
@@ -3,7 +3,7 @@ import { utils } from "./";
 import { useForm  } from "react-hook-form";
 export * from "react-hook-form";
 
-const Form = ({ children, onSubmit, onReset, onError, onInit, onWatch, onFormState, defaultValues, rules, formOptions, ...props }) => {
+const Form = ({ _usemodule_in_design, children, onSubmit, onReset, onError, onInit, onWatch, onFormState, defaultValues, rules, formOptions, ...props }) => {
   const id = React.useRef( utils.getRand() ),
     ref = React.useRef(),
     instance = useForm( formOptions ),
@@ -17,6 +17,7 @@ const Form = ({ children, onSubmit, onReset, onError, onInit, onWatch, onFormSta
     onFormState({ isDirty, isValid, dirtyFields, isSubmitting, isSubmitted, touched, submitCount, errors }, formState);
   }
   React.useEffect(() => {
+      if(!_usemodule_in_design){
       for ( let name, field, elements = ref.current.elements, i = 0, l = elements.length; i < l; i++ ) {
         field = elements[i];
         name = field.name;
@@ -25,10 +26,11 @@ const Form = ({ children, onSubmit, onReset, onError, onInit, onWatch, onFormSta
         }
       }
       if (onInit && typeof onInit === "function") onInit(ref.current, instance);
+      }
   }, [register, instance, onInit, rules]);
 
   return React.useMemo( () => {
-    return React.createElement("form",{ ...props, ref,
+    return React.createElement((_usemodule_in_design?"div":"form"),{ ...props, ref,
       onSubmit: handleSubmit(onSubmit, onError),
       onReset: () => {
         return reset(Object.assign({}, defaultValues));
