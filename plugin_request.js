e5d5aee3db332bb0537d8b7b87cb7f32860e3419
Jack Li <linb.net@gmail.com>
2020-12-08 02:09:32 -0500

    0.3.2

--- plugin_request.js
+++ plugin_request.js
@@ -1,14 +1,28 @@
 import React from "react";
 import axios from "axios";
-import { useModule } from "./";
+import { useModule, utils } from "./";
 
-let BASE_URL = "";
-let MOCK_FETCH = null;
-const setGlobalBaseURL = url => (BASE_URL = url);
-const setMockFetch = fun => (MOCK_FETCH = fun);
-
-const useRequest = ( url, params="", data="", method="get", baseURL="", AUTH_TOKEN="", header={}, config = {}, execute=false ) => {
+let GLOBAL_BASE_URL = "";
+let FETCH_MOCKER = null;
+const setGlobalBaseURL = baseURL => (GLOBAL_BASE_URL = baseURL);
+const setFetchMocker = mocker => (FETCH_MOCKER = mocker);
+const mergeConf = ( url, params, data, method, baseURL, AUTH_TOKEN, header, config ) =>{
   let source;
+  let conf = Object.assign({}, config);
+  url && (conf.url = url);
+  method && (conf.method = method);
+  conf.baseURL = ((baseURL || GLOBAL_BASE_URL || "") + "") || undefined;
+  params && (conf.params = params);
+  data && (conf.data = data);
+  conf.header = Object.assign({}, header, conf.header);
+  AUTH_TOKEN && (conf.header['Authorization'] = AUTH_TOKEN);
+  if(!conf.cancelToken){
+    source = axios.CancelToken.source();
+    conf.cancelToken = source.token;
+  }
+  return conf;
+};
+const useRequest = ( url, params="", data="", method="get", baseURL="", AUTH_TOKEN="", header={}, config = {}, execute=false ) => {
   const [status, setStatus] = React.useState("idle");
   const [error, setError] = React.useState(null);
   const [response, setResponse] = React.useState(null);
@@ -16,29 +30,23 @@ const useRequest = ( url, params="", data="", method="get", baseURL="", AUTH_TOK
   // eslint-disable-next-line
   React.useEffect(() => execute && fetch(), []);
 
-  config = Object.assign({}, config);
-  url && (config.url = url);
-  method && (config.method = method);
-  config.baseURL = ((baseURL || BASE_URL || "") + "") || undefined;
-  params && (config.params = params);
-  data && (config.data = data);
-  config.header = Object.assign({}, header, config.header);
-  AUTH_TOKEN && (config.header['Authorization'] = AUTH_TOKEN);
-  if(!config.cancelToken){
-    source = axios.CancelToken.source();
-    config.cancelToken = source.token;
-  }
+  let conf = mergeConf(url, params, data, method, baseURL, AUTH_TOKEN, header, config);
 
-  const fetch = () => {
-    if (config.url) {
+  const fetch = (config = null) => {
+    if(config && utils.isObject(config)){
+      conf = Object.assign({}, conf, config);
+    }
+    if(conf.url) {
       setStatus("loading");
-      (MOCK_FETCH || axios)(config).then(response => {
+      (FETCH_MOCKER || axios)(conf).then(response => {
         setResponse(response);
         setStatus("success");
       }).catch(error => {
         setError(error);
         setStatus(axios.isCancel(error)?"cancel":"error");
       });
+    }else{
+      setError(new Error('No url!'));
     }
   };
   const cancel = message => source && source.cancel(message);
@@ -47,19 +55,8 @@ const useRequest = ( url, params="", data="", method="get", baseURL="", AUTH_TOK
 
 useModule.refPlugIn("request", module => {
   return {
-    request: async ( url, data, method="get", baseURL="", AUTH_TOKEN="", header={}, config = {} ) => {
-      config = Object.assign({}, config);
-      url && (config.url = url);
-      method && (config.method = method);
-      baseURL && (config.baseURL = baseURL);
-        if(!config.method || config.method==="get"){
-          data && (config.params = data);
-        }else{
-          data && (config.data = data);
-        }
-        config.header = Object.assign({}, header, config.header);
-        AUTH_TOKEN && (config.header['Authorization'] = AUTH_TOKEN);
-      return await axios( config );
+    request: async ( url, params="", data="", method="get", baseURL="", AUTH_TOKEN="", header={}, config = {} ) => {
+      return await (FETCH_MOCKER || axios)( mergeConf(url, params, data, method, baseURL, AUTH_TOKEN, header, config) );
     }
   };
 });
@@ -74,4 +71,4 @@ useModule.statePlugIn("request", module => {
         ) : {};
 });
 
-export { useRequest, axios, setGlobalBaseURL, setMockFetch };
+export { useRequest, axios, setGlobalBaseURL, setFetchMocker };
